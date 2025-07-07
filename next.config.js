81f5d461d0d7e6984cfdfc1e0d110f00cd3b486b
Mercus005 <149353583+Mercus005@users.noreply.github.com>
2025-07-07 21:31:51 +0800

    d

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
- 
+ /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: isProd ? "/Next.js-Tailwind-CSS-Portfolio-Template" : "",
   assetPrefix: isProd ? "/Next.js-Tailwind-CSS-Portfolio-Template/" : "",
   output: "export",
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
